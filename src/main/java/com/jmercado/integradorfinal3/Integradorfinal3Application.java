21ee41ec61309c3455075e45da4d72a853bd0d4b
Juan Mercado <juan.mercado1986@gmail.com>
2024-03-13 18:35:18 -0300

    Primer Commit

---
+++ src/main/java/com/jmercado/integradorfinal3/Integradorfinal3Application.java
@@ -0,0 +1,13 @@
+package com.jmercado.integradorfinal3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Integradorfinal3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Integradorfinal3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmercado/integradorfinal3/Integradorfinal3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmercado.integradorfinal3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integradorfinal3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
